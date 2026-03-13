c165323df99e1b6a11fb0ca91fc878da29657971
Des1red <justbeasteasy@gmail.com>
2026-03-13 14:40:40 -0400

    hotfix

--- httpkit/env.go
+++ httpkit/env.go
@@ -13,8 +13,6 @@ func BuildRuntime(envPath string) Runtime {
 
 	return Runtime{
 		Dev:        os.Getenv("DEV") == "true",
-		APIDev:     os.Getenv("API_DEV"),
-		APIProd:    os.Getenv("API_PROD"),
 		DevOrigin:  os.Getenv("DEV_ORIGIN"),
 		ProdOrigin: os.Getenv("PROD_ORIGIN"),
 		Port:       ":" + internalenv.GetPort(),
--- internal/config/runtime.go
+++ internal/config/runtime.go
@@ -2,8 +2,6 @@ package config
 
 type Runtime struct {
 	Dev        bool
-	APIDev     string
-	APIProd    string
 	DevOrigin  string
 	ProdOrigin string
 	Port       string
